28dd9fee29739fa34e0adabc7fbc1d3b2632d5bf
amyshchyshen <korym85@gmail.com>
2020-01-29 22:11:08 +0200

    Updates

---
+++ precache-manifest.0d53271e4e4c4204e1b8ec493b0461ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338d0b5c4ed2856e61cdd583c416eb99",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6a8db44c9da8ab5ba1df",
+    "url": "/goit-react-hw-01-components/static/css/main.6a14b5f5.chunk.css"
+  },
+  {
+    "revision": "25adb0d56c875b736f96",
+    "url": "/goit-react-hw-01-components/static/js/2.dccd3b2b.chunk.js"
+  },
+  {
+    "revision": "6a8db44c9da8ab5ba1df",
+    "url": "/goit-react-hw-01-components/static/js/main.b76c221c.chunk.js"
+  },
+  {
+    "revision": "8a62e5976254088d1ff4",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.3f5955ef.js"
+  }
+]);
