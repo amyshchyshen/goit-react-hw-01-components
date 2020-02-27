b33a6039743b676b77a635e8c01072606ad8b000
amyshchyshen <korym85@gmail.com>
2020-02-27 23:12:29 +0200

    new deploy

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -2,38 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Stats.module.css';
 
-// const getRandomColor = () => {
-//   const colors = ['red', 'green', 'blue'];
-//   const color = colors[Math.floor(Math.random() * colors.length)];
-//   return {
-//     background: color,
-//   };
-// };
-
-// const Stats = ({ title, stats }) => {
-//   return (
-//     <section className={styles['stats-section']}>
-//       <h2 className={styles.title}>{title}</h2>
-
-//       <ul className={styles['stat-list']}>
-//         {stats.map(stat => {
-//           return (
-//             <li className={styles.item} key={stat.id} style={getRandomColor()}>
-//               <span className={styles.label}>{stat.label}</span>
-//               <span className={styles.percentage}>{`${stat.percentage}%`}</span>
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </section>
-//   );
-// };
-
-// Stats.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   stats: PropTypes.arrayOf(PropTypes.object).isRequired,
-// };
-
 const getRandomColor = () => {
   const rgb = [];
   for (let i = 0; i < 3; i += 1) {
